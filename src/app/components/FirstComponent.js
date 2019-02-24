a6fca2d57edde2668a575ea62675c9bf084039dc
Mayank <mittalmayank10@gmail.com>
2019-02-24 22:18:19 +0530

    [POC] - setup

---
+++ src/app/components/FirstComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+
+
+export class FirstComponent extends React.Component{
+	render() {
+		return(
+			<div>Hello</div>
+		);
+	} 
+}
---
+++ src/app/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+import { FirstComponent } from "./components/FirstComponent";
+
+render(<FirstComponent/>, window.document.getElementById("app"));
+
