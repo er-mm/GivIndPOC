c75c8c15c7d1582ea2c3f0cb696cc1a894f66b16
Mayank Mittal <mm772g@us.att.com>
2019-02-25 17:53:04 +0530

    [images] - uploading images

--- src/app/components/FirstComponent.js
+++ src/app/components/FirstComponent.js
@@ -1,11 +1,52 @@
 import React from "react";
 import { render } from "react-dom";
+//import fs from 'file-system';
+//const aa = require('file-system');
 
 
 export class FirstComponent extends React.Component{
+
+	constructor(props) {
+        super();
+        this.state = {
+			allData:[]
+        };
+       
+	  }
+	  componentDidMount(){
+    	//this.setState({ isLoading: true });
+        this.fetchData();
+	}
+	
+	fetchData(){
+    	
+    	fetch('http://starlord.hackerearth.com/insta')
+  	  .then(data => data.json())
+  	  .then(function(data) {
+  		  console.log('888888888888888888',data);
+  	    //console.log('55555555555555',JSON.stringify(myJson));
+  	    this.setState({ allData: data});
+  	  }.bind(this));
+    	
+    }
 	render() {
+		const renderTodos = this.state.allData.map((data, index) => {
+			// console.log('data : ',data);
+			  return (
+					
+						<div className="col-sm-2" key={index}>
+                            <img src={data.Image} alt="Italian Trulli"/>
+                        </div>
+				);
+			});
 		return(
-			<div>Hello</div>
+			<div className="container">
+                <div className="row">
+                        
+                       {renderTodos}                        
+                </div>
+            </div>
+
 		);
 	} 
 }
