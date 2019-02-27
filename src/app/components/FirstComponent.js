ff053cab477bb534477108b03e7034b448fbcc15
Mayank Mittal <mm772g@us.att.com>
2019-02-27 19:07:52 +0530

    [Image share] - alt changes

--- src/app/components/FirstComponent.js
+++ src/app/components/FirstComponent.js
@@ -123,7 +123,7 @@ export class FirstComponent extends React.Component{
 			  return (
 					<div className="card col-sm-3 marginAll" key={'img_'+index}>
 						<div className="imgPreview" >
-                            <img src={data.Image} onDoubleClick={this.clickHandle.bind(this,index)} alt={this.state.files.name ? this.state.files.name : 'Corrupted Image' }/>
+                            <img src={data.Image} onDoubleClick={this.clickHandle.bind(this,index)} alt={'Image'}/>
 														<svg id={index} className="heart" viewBox="0 0 32 29.6">
   <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
 	c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
